17d5ef3d823eb37562f1bfcdc484b99effc4f6c8
vsa <58674709+Vsa1968911450@users.noreply.github.com>
2022-05-09 13:50:00 +0800

    添加pinia学习目录

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+defineProps<{msg:string}>()
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
